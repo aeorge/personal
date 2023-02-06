853f547e9d9f509b1814eb3d6a98c5e4f4e3c092
Georgios Kotziabassis <kotziabassis@gmail.com>
2023-02-06 18:26:02 +0100

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { AnalyticsWrapper } from '@/components/analytics'
 import './globals.css'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -5,7 +6,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html className="bg-white text-black" lang="en">
       <head />
       <body className="touch-manipulation overflow-x-hidden antialiased">
-        <main className="my-8 max-w-3xl px-6 md:my-16 md:mx-auto lg:my-32">{children}</main>
+        <main className="my-8 max-w-3xl px-6 md:my-16 md:mx-auto lg:my-32">
+          {children}
+          <AnalyticsWrapper />
+        </main>
       </body>
     </html>
   )
