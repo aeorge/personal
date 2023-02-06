1f21b4eb1096c5d0498f97e5ecd485b29909ebe3
Georgios Kotziabassis <kotziabassis@gmail.com>
2023-02-06 23:45:01 +0100

    Use Inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,18 @@
 import { AnalyticsWrapper } from '@/components/analytics'
+import { Inter } from '@next/font/google'
 import './globals.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html className="bg-white text-[#141414] dark:bg-[#141414] dark:text-white" lang="en">
+    <html
+      className={`bg-white text-[#141414] dark:bg-[#141414] dark:text-white ${inter.variable}`}
+      lang="en"
+    >
       <head />
       <body className="touch-manipulation overflow-x-hidden antialiased">
         <main className="my-8 max-w-3xl px-6 md:my-16 md:mx-auto lg:my-32">
