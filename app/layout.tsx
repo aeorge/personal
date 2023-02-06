1266fcd92e16a70c93deb8d9d8b0be6144fe0fa0
Georgios Kotziabassis <kotziabassis@gmail.com>
2023-02-06 17:55:06 +0100

    Add content

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,11 @@ import './globals.css'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html className="bg-white text-black" lang="en">
       <head />
-      <body>{children}</body>
+      <body className="touch-manipulation overflow-x-hidden antialiased">
+        <main className="my-8 max-w-3xl px-6 md:my-16 md:mx-auto lg:my-32">{children}</main>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,50 @@
 export default function Page() {
-  return null
+  return (
+    <section>
+      <h1 className="text-2xl font-bold">Georgios Kotziabassis</h1>
+      <h2 className="text-xl">aeorge</h2>
+      <hr className="my-4 rounded border" />
+      <p className="mb-4">
+        Hello, friend! I am a software engineer at{' '}
+        <a
+          className="underline"
+          href="https://spacegoats.io/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          SPACEGOATS
+        </a>{' '}
+        where we aim to simplify international sales.
+      </p>
+      <p className="my-4">
+        I have a passion for web technologies and I am always eager to learn new things and expand
+        my skills and knowledge in this ever-evolving field.
+      </p>
+      <p className="my-4">
+        Aside from technology I love exploring new coffee shops and practicing my barista skills at
+        home. I also enjoy building and customizing mechanical keyboards.
+      </p>
+      <p className="mt-4">
+        If you have similar interests, I would love to connect with you on{' '}
+        <a
+          className="underline"
+          href="https://twitter.com/kotziabassis"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Twitter
+        </a>{' '}
+        and chat about all things technology, coffee and keyboards. You can also follow me on{' '}
+        <a
+          className="underline"
+          href="https://github.com/aeorge"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          GitHub
+        </a>{' '}
+        to stay up to date with my latest projects and contributions.
+      </p>
+    </section>
+  )
 }
