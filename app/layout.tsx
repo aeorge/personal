b1943a825354d7259293464edf6097a723aa9556
Georgios Kotziabassis <kotziabassis@gmail.com>
2023-02-06 23:26:21 +0100

    Add dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html className="bg-white text-black" lang="en">
+    <html className="bg-white text-[#141414] dark:bg-[#141414] dark:text-white" lang="en">
       <head />
       <body className="touch-manipulation overflow-x-hidden antialiased">
         <main className="my-8 max-w-3xl px-6 md:my-16 md:mx-auto lg:my-32">
