0fc00c580fb77d0f47eed1a3c1bc23be7957f742
Georgios Kotziabassis <kotziabassis@gmail.com>
2023-02-07 13:55:13 +0100

    Increase paragraph leading more

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
       <h1 className="text-2xl font-bold">Georgios Kotziabassis</h1>
       <h2 className="text-xl">aeorge</h2>
       <hr className="my-4 rounded border border-[#c3c3c3] dark:border-[#3c3c3c]" />
-      <p className="mb-4 leading-relaxed">
+      <p className="mb-4 leading-[1.75rem]">
         Hello, friend! I am a software engineer at{' '}
         <a
           className="underline underline-offset-2"
@@ -16,15 +16,15 @@ export default function Page() {
         </a>{' '}
         where we aim to simplify international sales.
       </p>
-      <p className="my-4 leading-relaxed">
+      <p className="my-4 leading-[1.75rem]">
         I have a passion for web technologies and I am always eager to learn new things and expand
         my skills and knowledge in this ever-evolving field.
       </p>
-      <p className="my-4 leading-relaxed">
+      <p className="my-4 leading-[1.75rem]">
         Aside from technology I love exploring new coffee shops and practicing my barista skills at
         home. I also enjoy building and customizing mechanical keyboards.
       </p>
-      <p className="mt-4 leading-relaxed">
+      <p className="mt-4 leading-[1.75rem]">
         If you have similar interests, I would love to connect with you on{' '}
         <a
           className="underline underline-offset-2"
